ad85956a646765352e2e5b9c20d457599c229c02
koreoxy <rayssankn@gmail.com>
2024-05-11 22:58:17 +0700

    fix : navbar and footer

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -1,41 +1,44 @@
-import { Github, Twitter, Youtube } from "lucide-react";
 import { Link } from "react-router-dom";
+import React from "react";
+import { Github, Twitter, Youtube } from "lucide-react";
 
-export const Footer = () => {
-  <footer className="border-t p-10">
-    <div className="mx-auto w-full max-w-3xl spacey-20">
-      <div className="flex flex-row justify-center items-center">
-        <div className="flex flex-col ">
-          <div className="font-bold text-3xl font-pacifico">
-            Calender-Hijriah
-          </div>
-          <div className="flex justify-center mt-5">
-            <Link
-              to="https://github.com/koreoxy"
-              className="p-2 text-gray-800 hover:text-[#ff0000]"
-            >
-              <Github />
-            </Link>
-            <Link
-              to="https://youtube.com/@1sh1sh"
-              className="p-2 text-gray-800 hover:text-[#ff0000]"
-            >
-              <Youtube />
-            </Link>
-            <Link
-              to="https://twitter.com/koreoxy"
-              className="p-2 text-gray-800 hover:text-[#ff0000]"
-            >
-              <Twitter />
-            </Link>
+export const Footer: React.FC = () => {
+  return (
+    <div className="border-t p-10">
+      <div className="mx-auto w-full max-w-3xl spacey-20">
+        <div className="flex flex-row justify-center items-center">
+          <div className="flex flex-col ">
+            <div className="font-bold text-3xl font-pacifico">
+              Calender-Hijriah
+            </div>
+            <div className="flex justify-center mt-5">
+              <Link
+                to="https://github.com/koreoxy"
+                className="p-2 text-gray-800 hover:text-[#ff0000]"
+              >
+                <Github />
+              </Link>
+              <Link
+                to="https://youtube.com/@1sh1sh"
+                className="p-2 text-gray-800 hover:text-[#ff0000]"
+              >
+                <Youtube />
+              </Link>
+              <Link
+                to="https://twitter.com/koreoxy"
+                className="p-2 text-gray-800 hover:text-[#ff0000]"
+              >
+                <Twitter />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="border-t mt-10">
-        <p className="text-center mt-1 text-sm">
-          &copy; Copyright 2024. Calender-Hijriah
-        </p>
+        <div className="border-t mt-10">
+          <p className="text-center mt-1 text-sm">
+            &copy; Copyright 2024. Calender-Hijriah
+          </p>
+        </div>
       </div>
     </div>
-  </footer>;
+  );
 };
